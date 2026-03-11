77d5656bae2bd300745e50a63047901558a5acac
ferpdf <42195393+ferpdf@users.noreply.github.com>
2026-03-11 01:19:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-document.querySelectorAll("a").forEach(link=>{
+document.querySelectorAll('a').forEach(link=>{
 
-link.addEventListener("mouseover",()=>{
+link.addEventListener('click',()=>{
 
-link.style.opacity="0.8"
-
-})
-
-link.addEventListener("mouseout",()=>{
-
-link.style.opacity="1"
+window.scrollTo(0,0)
 
 })
 
