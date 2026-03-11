850e62eef73c5f86f576cc801b9f03d7349ef0b5
ferpdf <42195393+ferpdf@users.noreply.github.com>
2026-03-11 00:10:29 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Animación scroll suave
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
